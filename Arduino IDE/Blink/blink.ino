f3bb7e94b616a4b463998a5e175efb9aeed55c98
Mr.Meng <mr.meng.eroxii@gmail.com>
2024-06-17 15:12:19 +0700

    Update blink.ino

--- Arduino IDE/Blink/blink.ino
+++ Arduino IDE/Blink/blink.ino
@@ -1,1 +1,14 @@
+// eRoxi TWIN STM32F1 Blink Arduinio IDE vai SWD
+const int ledPin = PC13; // Led Pin on the TWIN STM32 at pin PC13
+
+void setup() {
+  // Configure PC13 as output pin on TWIN STM32
+  pinMode( ledPin,OUTPUT);
+
+}
+
+void loop() {
+  digitalToggle(ledPin);
+  delay(1000); // wait for 1 seconde to blink LED
+}
 
